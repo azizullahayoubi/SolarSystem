e8c1d0873a9adc3473c378b5404dbd90e24f323c
azizullahayoubi <azizullahayoubi@gmail.com>
2021-06-13 12:23:30 -0400

    solar system structure is ready to deploy

--- src/com/seleniumDemo1/Main.java
+++ src/com/seleniumDemo1/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.sun=1;
+        FactorX.moon=3;
+        FactorX.planets=2;
+
     }
 }
--- src/com/seleniumDemo1/SolarSystem.java
+++ src/com/seleniumDemo1/SolarSystem.java
@@ -4,5 +4,5 @@ public class SolarSystem {
     int sun;
     int moon;
     int planets;
-    
+
 }
